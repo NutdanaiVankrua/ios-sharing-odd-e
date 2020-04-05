52d87f446460c5b9b68f5bf2fc837288d95b0645
Nutdanai Vankrua <nutdanai@odd.works>
2020-04-05 09:13:09 +0700

    Initial Commit

---
+++ OddsTeam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OddsTeam
+//
+//  Created by Nutdanai Vankrua on 5/4/2563 BE.
+//  Copyright © 2563 AQuery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OddsTeamTests/OddsTeamTests.swift
@@ -0,0 +1,34 @@
+//
+//  OddsTeamTests.swift
+//  OddsTeamTests
+//
+//  Created by Nutdanai Vankrua on 5/4/2563 BE.
+//  Copyright © 2563 AQuery. All rights reserved.
+//
+
+import XCTest
+@testable import OddsTeam
+
+class OddsTeamTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
