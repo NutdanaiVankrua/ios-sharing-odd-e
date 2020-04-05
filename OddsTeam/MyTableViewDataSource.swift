b624fcb9d677e71e428918273ef6cc6355fe813a
Nutdanai Vankrua <nutdanai@odd.works>
2020-04-05 15:44:21 +0700

    custom table view cell

--- OddsTeam/MyTableViewDataSource.swift
+++ OddsTeam/MyTableViewDataSource.swift
@@ -2,17 +2,21 @@ import UIKit
 
 class MyTableViewDataSource: NSObject, UITableViewDataSource, UITableViewDelegate {
 
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 100
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "item: \(indexPath.row)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyCustomCell
+
+        cell.label.text = "this is a \n very\nvery\nvery\nlong text"
+
+
         return cell
     }
 
--- OddsTeam/ViewController.swift
+++ OddsTeam/ViewController.swift
@@ -4,13 +4,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
-    private var myTableViewDataSource = MyTableViewDataSource()
+    private let myTableViewDataSource = MyTableViewDataSource()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.estimatedRowHeight = 40
+        tableView.rowHeight = UITableView.automaticDimension
+
         tableView.dataSource = myTableViewDataSource
         tableView.delegate = myTableViewDataSource
+
+        let nib = UINib(nibName: "MyCustomCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "myCell")
     }
 
 }
